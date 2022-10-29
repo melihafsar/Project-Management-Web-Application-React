087a658d80d9f82c31bb23d594fa151af169e223
Melih Afşar <melih_afsar@hotmail.com.tr>
2022-10-29 18:29:51 +0300

    drag and drop application created.

--- src/pages/ProjectBoard.jsx
+++ src/pages/ProjectBoard.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import DragNDrop from '../components/DragNDrop';
 import SideBar from "../components/SideBar";
+
+const data = [
+  {
+      title: "Group1",
+      items: ["1", "2", "3"]
+  },
+  {
+      title: "Group2",
+      items: ["4"]
+  },
+  {
+      title: "Group3",
+      items: ["7", "8"]
+  }
+]
+
+
 function ProjectBoard() {
   return (
     <>
       <SideBar />
       <section className="home-section">
-        <div className="text">ProjectBoard</div>
+          <h1 className='page-title'>Project Board</h1>
+        <div className="text board">
+          <DragNDrop data={ data }/>
+        </div>
       </section>
     </>
 
